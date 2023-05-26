d18cd3a557a00fef68be8c697ef9182da3da4341
josh4got <125082025+josh4got@users.noreply.github.com>
2023-05-25 19:18:40 -0600

    Controllers: File Structure
    - Added files to controller folder
    - Completed index.js files

---
+++ controllers/api/index.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+const userRoutes = require("./user-routes");
+const departmentsRoutes = require("./department-routes.js");
+const employeesRoutes = require("./employee-routes.js");
+const rolesRoutes = require("./role-routes.js");
+
+router.use("/departments", departmentsRoutes);
+router.use("/employees", employeesRoutes);
+router.use("/roles", rolesRoutes);
+router.use("/users", userRoutes);
+
+module.exports = router;
