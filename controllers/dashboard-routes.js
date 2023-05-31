e217bca27e7ceb8c0cf66d9c91fdf6abebc2c7ab
josh4got <125082025+josh4got@users.noreply.github.com>
2023-05-31 10:42:32 -0600

    DASHBOARD:partial/chart.js
    - Set up dependencies where needed for char.js
    - Modified dashboard route to pull info needed for chart.js
    - Set up dashboard partial for handlebars

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,9 +1,18 @@
 const withAuth = require("../utils/auth");
-
 const router = require("express").Router();
+const db = require("../models");
 
 router.get("/", withAuth, async (req, res) => {
-  res.render("dashboard");
+  try {
+    const employees = await db.employee.findAll({
+      include: [db.role], // Include the 'role' model in the query
+    });
+
+    res.render("dashboard", { employees });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 module.exports = router;
