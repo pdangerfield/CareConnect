badcbf3e49afa4ae37ccf4c1889964b0c114be45
josh4got <125082025+josh4got@users.noreply.github.com>
2023-05-30 19:52:08 -0600

    ROUTES:role-routes.js
    - Added create, update, and delete routes for roles
    - Fixed type in controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -3,16 +3,7 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const dashboardRoutes = require("./dashboard-routes.js");
 
-router.use("/dasboard", dashboardRoutes);
-router.use("/api", apiRoutes);
-
-module.exports = router;
-const router = require("express").Router();
-
-const apiRoutes = require("./api");
-const homeRoutes = require("./dashboard-routes");
-
-router.use("/", homeRoutes);
+router.use("/dashboard", dashboardRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
