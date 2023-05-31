b00bc146557d79f2fabfb4f95dc642d3da69ba43
josh4got <125082025+josh4got@users.noreply.github.com>
2023-05-30 18:54:01 -0600

    inclusion

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,7 +2,7 @@ const withAuth = require("../utils/auth");
 
 const router = require("express").Router();
 
-router.get("/", withAuth async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   res.render("dashboard");
 });
 
