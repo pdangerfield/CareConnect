c1b9b4bcb22605a86143f8fd59f00620f544825b
shru <shruthishekar@gmail.com>
2023-05-30 19:37:16 -0600

    controller-index.js

--- controllers/index.js
+++ controllers/index.js
@@ -7,12 +7,4 @@ router.use("/dasboard", dashboardRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
-const router = require("express").Router();
 
-const apiRoutes = require("./api");
-const homeRoutes = require("./dashboard-routes");
-
-router.use("/", homeRoutes);
-router.use("/api", apiRoutes);
-
-module.exports = router;
