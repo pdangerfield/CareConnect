91eab18d275e010c9037ba66ef603be1b6003cc1
josh4got <125082025+josh4got@users.noreply.github.com>
2023-06-02 11:38:27 -0600

    Routes:employees
    - Fixed typo to properly reference employee model
    - Renamed paths to work correctly

--- controllers/api/employee-routes.js
+++ controllers/api/employee-routes.js
@@ -1,23 +1,30 @@
 const withAuth = require("../../utils/auth");
-const { Employee } = require("../../models");
+const { employee } = require("../../models");
 
 const router = require("express").Router();
 
-router.get("/employees", withAuth, async (req, res) => {
-  // Retrieve all employees from the database
-  const employees = await Employee.findAll();
+router.get("/", async (req, res) => {
+  try {
+    // Retrieve employee data from the database
+    const employees = await employee.findAll();
 
-  res.render("employees", { employees });
+    // Render the "employees" template and pass the employee data
+    res.render("employees", { employees });
+  } catch (error) {
+    console.error(error);
+    // Handle the error appropriately
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 // Add an employee
-router.post("/employees", withAuth, async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
     // Extract form data
     const { first_name, last_name, role_id, manager_id } = req.body;
 
     // Create a new employee using Sequelize's create method
-    await Employee.create({
+    await employee.create({
       first_name,
       last_name,
       role_id,
@@ -34,27 +41,27 @@ router.post("/employees", withAuth, async (req, res) => {
 });
 
 // Update an employee
-router.put("/employees/:id", withAuth, async (req, res) => {
+router.put("/:id", withAuth, async (req, res) => {
   try {
     const { id } = req.params; // Extract the employee ID from the request parameters
     const { first_name, last_name, role_id, manager_id } = req.body; // Extract updated employee data from the request body
 
     // Find the employee by ID
-    const employee = await Employee.findByPk(id);
+    const employeeRecord = await employee.findByPk(id);
 
     // If the employee doesn't exist, return an error response
-    if (!employee) {
+    if (!employeeRecord) {
       return res.status(404).send("Employee not found");
     }
 
     // Update the employee's properties
-    employee.first_name = first_name;
-    employee.last_name = last_name;
-    employee.role_id = role_id;
-    employee.manager_id = manager_id;
+    employeeRecord.first_name = first_name;
+    employeeRecord.last_name = last_name;
+    employeeRecord.role_id = role_id;
+    employeeRecord.manager_id = manager_id;
 
     // Save the updated employee to the database
-    await employee.save();
+    await employeeRecord.save();
 
     // Redirect to employees page or send a success response
     res.redirect("/employees");
@@ -66,20 +73,20 @@ router.put("/employees/:id", withAuth, async (req, res) => {
 });
 
 // Delete an employee
-router.delete("/employees/:id", withAuth, async (req, res) => {
+router.delete("/:id", withAuth, async (req, res) => {
   try {
     const { id } = req.params; // Extract the employee ID from the request parameters
 
     // Find the employee by ID
-    const employee = await Employee.findByPk(id);
+    const employeeRecord = await employee.findByPk(id);
 
     // If the employee doesn't exist, return an error response
-    if (!employee) {
+    if (!employeeRecord) {
       return res.status(404).send("Employee not found");
     }
 
     // Delete the employee from the database
-    await employee.destroy();
+    await employeeRecord.destroy();
 
     // Redirect to employees page or send a success response
     res.redirect("/employees");
