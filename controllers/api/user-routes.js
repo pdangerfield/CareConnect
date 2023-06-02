7367a5d38f7477a4456d8234bf2c307e6341fbf2
shru <shruthishekar@gmail.com>
2023-06-01 21:36:57 -0600

    modification

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,16 +1,13 @@
 const router = require("express").Router();
-const { User } = require("../../models/User");
+const { User } = require("../../models");
 
 // CREATE new user
 router.post("/", async (req, res) => {
   try {
-    const dbUserData = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
+    const dbUserData = await User.create(req.body);
 
     req.session.save(() => {
+      req.session.user_id = dbUserData.id;
       req.session.loggedIn = true;
 
       res.status(200).json(dbUserData);
