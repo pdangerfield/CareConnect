35cf119f36ee00cde9be32304ea91736acfaf4b4
Phil Dangerfield <philipdangerfield@Philips-MacBook-Pro.local>
2023-06-01 18:57:01 -0600

    added homeRoute to the controller

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+
+router.get('/', async (req, res) => {
+  try {
+   
+
+    res.render('homepage', {
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+);
+
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+
+module.exports = router;
