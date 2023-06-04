3b30b6cdedf060a4749593f0113426642d964ddf
josh4got <125082025+josh4got@users.noreply.github.com>
2023-06-04 16:02:32 -0600

    fixed pathing on routes/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,6 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const dashboardRoutes = require("./dashboard-routes.js");
 const homeRoutes = require("./homeRoutes.js");
-const departmentRoutes = require("./department-routes.js");
 
 router.use("/", homeRoutes);
 router.use("/dashboard", dashboardRoutes);
