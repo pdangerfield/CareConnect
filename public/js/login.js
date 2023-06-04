580999f7d5dce807f352646e3a4bea4271b9480f
shru <shruthishekar@gmail.com>
2023-06-04 15:18:29 -0600

    added signup and login

--- public/js/login.js
+++ public/js/login.js
@@ -19,32 +19,9 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const registerFormHandler = async (event) => {
-  event.preventDefault();
-
-  const username = document.querySelector("#name-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
-
-  if (username && email && password) {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({ username, email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      document.location.replace("/dashboard");
-    } else {
-      alert("Failed to sign up.");
-    }
-  }
-};
 
 document
   .querySelector(".login-form")
   .addEventListener("submit", loginFormHandler);
 
-document
-  .querySelector(".register-form")
-  .addEventListener("submit", registerFormHandler);
+
