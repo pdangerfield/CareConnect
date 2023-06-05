06d2419c23972a9beb24a647c484318ec6a6504f
Phil Dangerfield <philipdangerfield@Philips-MacBook-Pro.local>
2023-06-05 10:34:40 -0600

    adjusted user-routes so that log in and signup work properly. Still need to get routes working to redirect to correct pages when logged in

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,63 +1,55 @@
-const router = require("express").Router();
-const { User } = require("../../models");
-const bcrypt = require("bcrypt");
+const router = require('express').Router();
+const { User } = require('../../models');
 
-// CREATE new user
-router.post("/register", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const dbUserData = await User.create({
-      ...req.body,
-      password: hashedPassword,
-    });
+    const userData = await User.create(req.body);
 
     req.session.save(() => {
-      req.session.user_id = dbUserData.id;
+      req.session.user_id = userData.id;
       req.session.loggedIn = true;
 
-      res.status(200).json(dbUserData);
+      res.status(200).json(userData);
     });
   } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
-//   Login
-router.post("/", async (req, res) => {
+
+router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
         .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
+        .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      userData.password
-    );
+
+    const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
+        .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.json({ user: userData, message: "You are now logged in!" });
+      req.session.loggedIn = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
     });
+
   } catch (err) {
     res.status(400).json(err);
   }
 });
-// logout
-router.post("/logout", (req, res) => {
-  if (req.session.logged_in) {
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
     });
