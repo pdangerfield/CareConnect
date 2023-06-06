9b615ad6d096a7e77bd8199a6358d33e1bdd547d
shru <shruthishekar@gmail.com>
2023-06-05 18:55:14 -0600

    new employee
    - Added vies for new employee with,add,update and delete
    - Added js for add,edit and delete employee
    -

---
+++ public/js/edit-emp.js
@@ -0,0 +1,29 @@
+const editEmpFormHandler = async (event) => {
+    event.preventDefault();
+
+    const first_name = document.querySelector('input[name="first_name"]').value.trim();
+    const last_name = document.querySelector('input[name="last_name"]').value.trim();
+    const title = document.querySelector('select[name="title"]').value.trim();
+    
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length -1
+    ];
+
+    if (first_name && last_name && title) {
+        const response = await fetch(`/api/employees/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({ first_name, last_name, title }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            document.location.replace('/api/employees/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+    else {
+        alert("please enter name  and role for the department");
+    }
+}
+
+document.querySelector('.edit-employee-form').addEventListener('submit', editEmpFormHandler);
