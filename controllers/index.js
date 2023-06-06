864f67f604911faaff167290b213f79b669471d5
Phil Dangerfield <philipdangerfield@Philips-MacBook-Pro.local>
2023-06-05 19:24:07 -0600

    adjusted file structure and add routes for get login and signup

--- controllers/index.js
+++ controllers/index.js
@@ -6,4 +6,22 @@ const dashboardRoutes = require("./dashboard-routes.js");
 router.use("/dashboard", dashboardRoutes);
 router.use("/api", apiRoutes);
 
+router.get("/", async (req, res) => {
+    try {
+      res.render("homepage");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("An error occurred while retrieving roles.");
+    }
+  });
+
+router.get("/signup", async (req, res) => {
+    try {
+        res.render("signup");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occurred while retrieving roles.");
+    }
+});
+
 module.exports = router;
